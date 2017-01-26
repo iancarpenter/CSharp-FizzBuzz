e6aec1eacd0668fa97db78551361dc175104d1ae
Ian Carpenter <ianhcarpenter@gmail.com>
2017-01-26 21:57:03 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace FizzBuzz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                bool fizz = (i % 3) == 0;
+                bool buzz = (i % 5) == 0;
+                
+                if (fizz == true && buzz == true)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz == true)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz == true)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+
+            Console.Read();
+
+        }
+    }
+}
